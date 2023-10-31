95028b271872dc2010e00a198c376ea2491732ce
SahirRadzi <sahir.radzi@gmail.com>
2023-10-31 18:37:48 +0800

    Installation

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="#"><i class="fas fa-phone"></i><span>+601133165639</span></a>
+         <a href="#"><i class="fas fa-phone"></i><span>+60123456789</span></a>
+         <a href="#"><i class="fas fa-envelope"></i><span>2ndhome@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>32040, Seri Manjung Perak.</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>SewaBeliRumah</span></div>
+
+</footer>
+
+<!-- footer section ends -->
